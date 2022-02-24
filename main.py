ac633c4e800476f45d94c7d2ac02c2f69dec184a
AHARTMAN380 <42577436+AHARTMAN380@users.noreply.github.com>
2022-02-23 21:08:17 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+#Alex Hartman
+#TCMG 272-501
+#Septmeber 7th, 2021
+print("Howdy world")
+#Display:  Your name, class year, and favorite color on three separate lines
+print("Alex Hartman \n 2023 \n Green or Blue", end = '\n')
+#Display: Your favorite color, animal, and singer on three separate lines.
+print("Blue or green \n Wolf \n  Israel 'IZ' Kamakawiwoʻole")
+#end program
